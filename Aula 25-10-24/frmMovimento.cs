e5e1c76f186ce150c1fd07494d7a217a9f2e50c7
ddasni <danielsousamelo5@gmail.com>
2024-11-01 20:44:03 -0300

    Adicionando o comando insert do mysql na ação do botão incluir do frmMovimento

--- Aula 25-10-24/frmMovimento.cs
+++ Aula 25-10-24/frmMovimento.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -32,7 +33,33 @@ namespace Aula_25_10_24
 
         private void btnIncluir_Click(object sender, EventArgs e)
         {
-            
+            //TEM QUE DEFINIR A FK COD_PROD, FAZENDO ELA SER PUXADA DA OUTRA TABELA
+
+            string tipo = rdbEntrada.Checked ? "Entrada" : "Saida";
+
+            cmdSql.Clear();
+            cmdSql.Append("INSERT INTO Movto (cod_movto_fk, dt_movto, tipo, quant, obs) VALUES (@cod_movto, @dt_movto, @tipo, @quant, @obs)");
+
+            using (MySqlCommand cmd = new MySqlCommand(cmdSql.ToString()))
+            {
+                // Adiciona os parâmetros
+                cmd.Parameters.AddWithValue("@cod_movto_fk", txtCodMovto.Text);
+                cmd.Parameters.AddWithValue("@dt_movto", dateMovto.Value);
+                cmd.Parameters.AddWithValue("@tipo", tipo);
+                cmd.Parameters.AddWithValue("@quant", txtQuant.Text);
+                cmd.Parameters.AddWithValue("@obs", txtObservacoes.Text);
+
+
+
+                if (Conexao.ExecutarCmd(cmd) > 0)
+                {
+                    MessageBox.Show("Cadastro feito com sucesso");
+                }
+                else
+                {
+                    MessageBox.Show("O cadastro não deu certo");
+                }
+            }
         }
     }
 }
